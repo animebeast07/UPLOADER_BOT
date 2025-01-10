b352f36d843174298a08e17ff9cc5045fd14468c
animebeast07 <narutouzumakitamil2005@gmail.com>
2025-01-10 10:12:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID", "26169469")
     API_HASH = os.environ.get("API_HASH", "1e2225f3d65b401d7d5bb921af531712")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7521100361:AAHaWW6dJleX76xV4yLI9jog5YLgocY8SaQ") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
     DB_URL = os.environ.get("DB_URL", "mongodb+srv://Animebeasttamil:animebeast6374@cluster0.qzrwt.mongodb.net/")
     DB_NAME = os.environ.get("DB_NAME", "Animebeasttamil")
