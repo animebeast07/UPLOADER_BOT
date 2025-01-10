a7defb1bd5f2b033d181ae6b9232566b040c8e73
animebeast07 <narutouzumakitamil2005@gmail.com>
2025-01-10 10:12:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = os.environ.get("API_ID", "26169469")
+    API_HASH = os.environ.get("API_HASH", "1e2225f3d65b401d7d5bb921af531712")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://Animebeasttamil:animebeast6374@cluster0.qzrwt.mongodb.net/")
+    DB_NAME = os.environ.get("DB_NAME", "Animebeasttamil")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5326198063').split()]
 
 
 class temp(object): 
